a57dfe4cbb7141d0a34530332840cc2fd842de1e
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-15 15:35:50 -0500

    added extra fields for sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_birth, :gender, :user_img])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_birth, :gender, :user_img])
+  end
 end
