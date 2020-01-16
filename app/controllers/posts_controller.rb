67803b68fc131e1fdb11dc73bf1eb1d33e199658
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-16 10:19:20 -0500

    Posts controller generated

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/index'
   devise_for :users
   root 'static_pages#home' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
