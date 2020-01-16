0174a7de0c38dd1a155ddaae4fdcdc574aa923d0
lhscar <gagc056@gmail.com>
2020-01-16 17:28:20 -0300

    the comments controller was implemented

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,2 +1,37 @@
 class CommentController < ApplicationController
+    def  new
+      @comment=Comment.new
+    
+    end
+
+    def create
+        @post=Post.find_by(id: params[:id])
+        @comment=@post.comments.build(comment_params)
+        if @comment.save
+            flash[:success]='Your comment was successfully writted'
+            redirect_to @post
+        else
+            flash.now[:error]='Your comment couldn´t be writted'
+            render 'new'
+        end
+
+    end
+
+    def  index
+        @post=Post.find_by(id: params[:id])
+        @comment=@post.comments.all(comment_params)
+        if @comment.save
+            flash[:success]='Your comment was successfully writted'
+            redirect_to @post
+        else
+            flash.now[:error]='Your comment couldn´t be writted'
+            render 'new'
+        end
+    end
+
+    private
+
+    def comment_params
+      params.permit(:content)
+    end
 end
