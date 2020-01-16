da181eeae7620a1b002bd0fa50279686fe5c4dd5
lhscar <gagc056@gmail.com>
2020-01-15 22:02:28 -0300

    testing the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates if the user attributes are correct' do
+    user=User.new(first_name:'ed', last_name:'morales', email:'', password:'', date_birth:13/10/1990, gender:'male' )
+    expect(user).to be_invalid
+  end
 end
