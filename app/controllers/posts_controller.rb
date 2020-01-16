c1e4481f79b7c139ff4c908bea4d4b6d58d35b7a
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-16 10:25:09 -0500

    methods in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,34 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
   def new
+    @post = Post.new
+  
+  end
+
+  def create
+    @user = User.find_by(id: current_user.id)
+    @post = @user.post.build(post_params)
+    if @post.save
+      flash[:success] = "Post created"
+      redirect_to posts_path
+    else
+      flash.now[:error] = "Post can't be created"
+      render 'new'
+    end
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
-  def index
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
+
 end
