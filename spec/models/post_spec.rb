582a4d20d1fcc882a827c12991caded1c3b9d2fb
lhscar <gagc056@gmail.com>
2020-01-15 22:37:50 -0300

    testing the post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.create(first_name:'Carlos', last_name:'Tevez', email:'tevez@gmail.com', password:'12345', password_confirmation:'12345', date_birth:19/7/1980, gender:'male')
+  end
+  it 'validate it has a creator' do
+    post=user.posts.build(content: 'welcome to facebook')
+    expect(post.user.id).to eq(user.id)
+  end
 end
