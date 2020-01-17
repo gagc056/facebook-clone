091a635844986549af1639b31eb666db03a0e0d9
lhscar <gagc056@gmail.com>
2020-01-17 17:23:20 -0300

    fixing the errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it 'validates post presence' do
-    comment=Coment.new
+    comment = Coment.new
     comment.save
     expect(comment).to be_invalid
   end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   it 'validates a user presence' do
-    like=Like.new
+    like = Like.new
     like.save
     expect(like).to be_invalid
   end
