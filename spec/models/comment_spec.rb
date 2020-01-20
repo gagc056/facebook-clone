b38ef8da29546be6ea126982967595c81be8af51
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-20 17:19:25 -0500

    Testing case for Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it 'validates post presence' do
-    comment = Coment.new
+    comment = Comment.new
     comment.save
     expect(comment).to be_invalid
   end
