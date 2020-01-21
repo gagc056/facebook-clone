508b8d61864f5bc41c2175ad864304566c717342
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-21 14:39:34 -0500

    integration

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
+  sequence(:email) { |n| "jhon45556666#{n}@example.com" }
   factory :user do
-    first_name {'Jhon'}
-    last_name {'Doe'}
-    sequence(:email) { |n| "person#{n}@example.com" }
-    password {'12345678'}
-    password_confirmation {'12345678'}
-    date_birth {'03/02/1989'}
-    gender {'male'}
+    first_name { 'wwwwwww' }
+    last_name { 'dass' }
+    email
+    password { '12345678' }
+    password_confirmation { '12345678' }
+    date_birth { '03/02/1989' }
+    gender { 'male' }
   end
 end
