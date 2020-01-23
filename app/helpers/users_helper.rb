c511f3e8fc51696b5ff23562b575e575913170e2
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-23 16:04:47 -0500

    distinction with the current_user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module UsersHelper
+
+  def search_users_list users
+    users.select {|user| user unless user == current_user || current_user.friends.include?(user) }
+  end
+
+  def search_users_list_not_empty
+    search_users_list(@users).size > 0
+  end
 end
