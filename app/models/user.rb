7ae5a40fb36c11e9f1a354cf853447404c155804
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-24 16:08:56 -0500

    rubocop offenses solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   has_many :posts
   has_many :comments
@@ -32,22 +32,22 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.name.split(' ')[0]
-      user.last_name = auth.info.name.split(' ')[1]   # assuming the user model has a name
+      user.last_name = auth.info.name.split(' ')[1] # assuming the user model has a name
       user.user_img = auth.info.image # assuming the user model has an image
     end
   end
-  
+
   def show_post
     @friend_post = friends.map(&:posts).flatten
     @friend_post += posts
