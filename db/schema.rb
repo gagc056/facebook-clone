01f01c1bbf81644b93e2a67a2016d2b657633d4e
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-24 15:55:06 -0500

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_01_24_005946) do
-=======
 ActiveRecord::Schema.define(version: 2020_01_24_162335) do
->>>>>>> omniauth
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,16 +79,9 @@ ActiveRecord::Schema.define(version: 2020_01_24_162335) do
     t.string "user_img"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
-    t.string "provider"
-    t.string "uid"
-    t.string "name"
-    t.text "image"
-=======
     t.string "uuid"
     t.string "provider"
     t.string "uid"
->>>>>>> omniauth
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
