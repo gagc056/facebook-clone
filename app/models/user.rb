0fa1e8a73154b457fec1b0e2b421a8dd16e2e77c
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-27 15:10:23 -0500

    solved issue when user wanna sign in with Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,8 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+      data = session['devise.facebook_data']
+      if data && session['devise.facebook_data']['extra']['raw_info']
         user.email = data['email'] if user.email.blank?
       end
     end
